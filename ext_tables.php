8da31453766e2c3eb0f7a4cb2c16a1e959585aae
Kalmok <fischer-dreileben@t-online.de>
2016-11-16 10:16:03 +0100

    Revert "Version  1.1.0"
    This reverts commit 77b0df501b04fba77677965bb18ac671a408b51f.

--- ext_tables.php
+++ ext_tables.php
@@ -14,23 +14,22 @@ if(!defined('TYPO3_MODE')){
 $pluginSignature = 'datecblog_blog';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/ControllerSelection.xml');
-
+/*
 if (TYPO3_MODE == 'BE') {
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-			'Datec.' . $_EXTKEY,
-			'web',
-			'DatecBlogM1',
-			'', # Position
-			array(
-					'BlogmanagementModule' => 'overview, listComments, showBlog, blockCommentary, deleteCommentary, showSinglePost, listPosts, listBlogComments'), # Controller array
-			array(
-					'access' => 'user,group',
-					'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/Backend/Icons/DatecBlogM1.png',
-					'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf',
-			)
-			);
+		'Datec.' . $_EXTKEY,
+		'web',
+		'DatecBlogM1',
+		'', # Position
+		array('BlogmanagementModule' => 'posts,keywords,commentaryCreators'), # Controller array
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Backend/Icons/DatecBlogM1.png',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf',
+		)
+	);		
 }
-
+*/
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_datecblog_domain_model_post');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_datecblog_domain_model_category');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_datecblog_domain_model_keyword');
